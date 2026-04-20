8e3d5247030078de051dfdc0a6e95bd6428700e3
Math Charles N'guessan <mathcharlesnguessan@gmail.com>
2026-04-20 10:33:38 +0000

    script extraction

---
+++ src/extract.py
@@ -0,0 +1,39 @@
+"""
+extract.py
+extractions des  données brutes de notre fichiers csv source
+Responsabilité unique : lire sans modifier
+"""
+
+import pandas as pd
+import logging
+from pathlib import Path
+
+## configuration du logging
+logging.basicConfig(
+    level = logging.INFO,   ## affiches les messages infos , warnig , error ...
+    format = "%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+    datefmt = "%d-%m-%Y %H:%M:%S"
+)
+
+## creer un nom de logger pour identifier la source du message
+logger = logging.getLogger("ETL.extract")
+
+## fonction principal : pend un chemin et retourne un dataframe
+def extract_from_csv(file_path:str) -> pd.DataFrame:
+    path = Path("/home/charles-nguessan/Projects/elt_debutant/data/sales_raw.csv")
+
+    ## gestion des erreurs
+
+    if not path.exists():       ## verifie que le fichiers existe
+        raise FileNotFoundError(f"fichier source introuvalble : {file_path}")
+
+    if path.stat().st.size == 0 :       ## verifie que le fichier n'est pas vide
+        raise ValueError(f"fichier source vide : {file_path}")
+
+    logger.info(f"extraction des lignes depuis : {file_path}")
+
+
+
+
+
+
